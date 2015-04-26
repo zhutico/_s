1e03c8f84e3f6d9fafe65897607f7df58bcf8312
zhutico <zhutico@qq.com>
2015-04-26 15:24:57 +0800

    添加中文翻译

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying the footer.
+ * 底部模板
  *
- * Contains the closing of the #content div and all content after
+ * 包含在#content关闭之后和所有内容之后
  *
  * @package _s
  */
@@ -12,9 +12,10 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', '_s' ) ); ?>"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', '_s' ) ); ?>"><?php printf( __( '自豪地采用 %s', '_s' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( __( '主题：%1$s by %2$s.', '_s' ), '_s', '<a href="http://www.zhutico.com/dev/createtheme
+ " rel="designer">主题酷</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
