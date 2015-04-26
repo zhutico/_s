882dc4ec5a320709fd7aed85fc7da875dcf0c33e
zhutico <zhutico@qq.com>
2015-04-26 22:33:20 +0800

    添加中文翻译

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * Custom template tags for this theme.
+ * 标签模板
  *
- * Eventually, some of the functionality here could be replaced by core features.
+ * 一些函数将会替换WordPress的核心功能
  *
  * @package _s
  */
 
 if ( ! function_exists( 'the_posts_navigation' ) ) :
 /**
- * Display navigation to next/previous set of posts when applicable.
+ * 显示上一页/下一页分页
  *
- * @todo Remove this function when WordPress 4.3 is released.
+ * @todo WordPress 4.3发布时移除这个函数
  */
 function the_posts_navigation() {
-	// Don't print empty markup if there's only one page.
+	// 如果只有一页时不打印空标签
 	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
 		return;
 	}
@@ -39,9 +39,9 @@ endif;
 
 if ( ! function_exists( 'the_post_navigation' ) ) :
 /**
- * Display navigation to next/previous post when applicable.
+ * 显示上一页/下一页分页
  *
- * @todo Remove this function when WordPress 4.3 is released.
+ * @todo WordPress 4.3发布时移除这个函数
  */
 function the_post_navigation() {
 	// Don't print empty markup if there's nowhere to navigate.
@@ -53,7 +53,7 @@ function the_post_navigation() {
 	}
 	?>
 	<nav class="navigation post-navigation" role="navigation">
-		<h2 class="screen-reader-text"><?php _e( 'Post navigation', '_s' ); ?></h2>
+		<h2 class="screen-reader-text"><?php _e( '文章分页', '_s' ); ?></h2>
 		<div class="nav-links">
 			<?php
 				previous_post_link( '<div class="nav-previous">%link</div>', '%title' );
@@ -67,7 +67,7 @@ endif;
 
 if ( ! function_exists( '_s_posted_on' ) ) :
 /**
- * Prints HTML with meta information for the current post-date/time and author.
+ * 在meta中打印当前文章发布日期/时间，作者信息
  */
 function _s_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
@@ -83,12 +83,12 @@ function _s_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		_x( 'Posted on %s', 'post date', '_s' ),
+		_x( '%s', '发布于', '_s' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
 	$byline = sprintf(
-		_x( 'by %s', 'post author', '_s' ),
+		_x( '%s', '作者', '_s' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
@@ -99,92 +99,92 @@ endif;
 
 if ( ! function_exists( '_s_entry_footer' ) ) :
 /**
- * Prints HTML with meta information for the categories, tags and comments.
+ * 在meta中打印分类，标签和评论
  */
 function _s_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' == get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
+		/* 翻译：翻译逗号之后*/
 		$categories_list = get_the_category_list( __( ', ', '_s' ) );
 		if ( $categories_list && _s_categorized_blog() ) {
-			printf( '<span class="cat-links">' . __( 'Posted in %1$s', '_s' ) . '</span>', $categories_list );
+			printf( '<span class="cat-links">' . __( '发布于 %1$s', '_s' ) . '</span>', $categories_list );
 		}
 
-		/* translators: used between list items, there is a space after the comma */
+		/* 翻译：翻译逗号之后*/
 		$tags_list = get_the_tag_list( '', __( ', ', '_s' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . __( 'Tagged %1$s', '_s' ) . '</span>', $tags_list );
+			printf( '<span class="tags-links">' . __( '标签%1$s', '_s' ) . '</span>', $tags_list );
 		}
 	}
 
 	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link">';
-		comments_popup_link( __( 'Leave a comment', '_s' ), __( '1 Comment', '_s' ), __( '% Comments', '_s' ) );
+		comments_popup_link( __( '留下您的评论', '_s' ), __( '1条评论', '_s' ), __( '%条评论', '_s' ) );
 		echo '</span>';
 	}
 
-	edit_post_link( __( 'Edit', '_s' ), '<span class="edit-link">', '</span>' );
+	edit_post_link( __( '编辑', '_s' ), '<span class="edit-link">', '</span>' );
 }
 endif;
 
 if ( ! function_exists( 'the_archive_title' ) ) :
 /**
- * Shim for `the_archive_title()`.
+ * the_archive_title()
  *
- * Display the archive title based on the queried object.
+ * 基于查询对象显示存档页面标题
  *
- * @todo Remove this function when WordPress 4.3 is released.
+ * @todo WordPress 4.3发布时移除这个函数
  *
- * @param string $before Optional. Content to prepend to the title. Default empty.
- * @param string $after  Optional. Content to append to the title. Default empty.
+ * @param string $before 可选，内容的标题开头，默认的空
+ * @param string $after  可选，内容添加到标题，默认的空
  */
 function the_archive_title( $before = '', $after = '' ) {
 	if ( is_category() ) {
-		$title = sprintf( __( 'Category: %s', '_s' ), single_cat_title( '', false ) );
+		$title = sprintf( __( '分类：%s', '_s' ), single_cat_title( '', false ) );
 	} elseif ( is_tag() ) {
-		$title = sprintf( __( 'Tag: %s', '_s' ), single_tag_title( '', false ) );
+		$title = sprintf( __( '标签：%s', '_s' ), single_tag_title( '', false ) );
 	} elseif ( is_author() ) {
-		$title = sprintf( __( 'Author: %s', '_s' ), '<span class="vcard">' . get_the_author() . '</span>' );
+		$title = sprintf( __( '作者： %s', '_s' ), '<span class="vcard">' . get_the_author() . '</span>' );
 	} elseif ( is_year() ) {
-		$title = sprintf( __( 'Year: %s', '_s' ), get_the_date( _x( 'Y', 'yearly archives date format', '_s' ) ) );
+		$title = sprintf( __( '年：%s', '_s' ), get_the_date( _x( 'Y', '每年的文章存档的日期格式', '_s' ) ) );
 	} elseif ( is_month() ) {
-		$title = sprintf( __( 'Month: %s', '_s' ), get_the_date( _x( 'F Y', 'monthly archives date format', '_s' ) ) );
+		$title = sprintf( __( '月： %s', '_s' ), get_the_date( _x( 'F Y', '每月的文章存档的日期格式', '_s' ) ) );
 	} elseif ( is_day() ) {
-		$title = sprintf( __( 'Day: %s', '_s' ), get_the_date( _x( 'F j, Y', 'daily archives date format', '_s' ) ) );
+		$title = sprintf( __( 'Day: %s', '_s' ), get_the_date( _x( 'F j, Y', '每天的文章存档的日期格式', '_s' ) ) );
 	} elseif ( is_tax( 'post_format' ) ) {
 		if ( is_tax( 'post_format', 'post-format-aside' ) ) {
-			$title = _x( 'Asides', 'post format archive title', '_s' );
+			$title = _x( 'Aside', '文章存档页面文章标题', '_s' );
 		} elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
-			$title = _x( 'Galleries', 'post format archive title', '_s' );
+			$title = _x( 'Galleries', '文章存档页面文章标题', '_s' );
 		} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
-			$title = _x( 'Images', 'post format archive title', '_s' );
+			$title = _x( 'Images', '文章存档页面文章标题', '_s' );
 		} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
-			$title = _x( 'Videos', 'post format archive title', '_s' );
+			$title = _x( 'Videos', '文章存档页面文章标题', '_s' );
 		} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
-			$title = _x( 'Quotes', 'post format archive title', '_s' );
+			$title = _x( 'Quotes', '文章存档页面文章标题', '_s' );
 		} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
-			$title = _x( 'Links', 'post format archive title', '_s' );
+			$title = _x( 'Links', '文章存档页面文章标题', '_s' );
 		} elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
-			$title = _x( 'Statuses', 'post format archive title', '_s' );
+			$title = _x( 'Statuses', '文章存档页面文章标题', '_s' );
 		} elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
-			$title = _x( 'Audio', 'post format archive title', '_s' );
+			$title = _x( 'Audio', '文章存档页面文章标题', '_s' );
 		} elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
-			$title = _x( 'Chats', 'post format archive title', '_s' );
+			$title = _x( 'Chats', '文章存档页面文章标题', '_s' );
 		}
 	} elseif ( is_post_type_archive() ) {
-		$title = sprintf( __( 'Archives: %s', '_s' ), post_type_archive_title( '', false ) );
+		$title = sprintf( __( '文章存档：%s', '_s' ), post_type_archive_title( '', false ) );
 	} elseif ( is_tax() ) {
 		$tax = get_taxonomy( get_queried_object()->taxonomy );
-		/* translators: 1: Taxonomy singular name, 2: Current taxonomy term */
+		/* 翻译： 1：文章标题自定义分类, 2：当前分类 */
 		$title = sprintf( __( '%1$s: %2$s', '_s' ), $tax->labels->singular_name, single_term_title( '', false ) );
 	} else {
-		$title = __( 'Archives', '_s' );
+		$title = __( '文章存档', '_s' );
 	}
 
 	/**
-	 * Filter the archive title.
+	 * 存档页面标题
 	 *
-	 * @param string $title Archive title to be displayed.
+	 * @param string $title 显示的存档页面标题
 	 */
 	$title = apply_filters( 'get_the_archive_title', $title );
 
@@ -196,25 +196,25 @@ endif;
 
 if ( ! function_exists( 'the_archive_description' ) ) :
 /**
- * Shim for `the_archive_description()`.
+ * the_archive_description()
  *
- * Display category, tag, or term description.
+ * 显示分类，标签或者描述
  *
- * @todo Remove this function when WordPress 4.3 is released.
+ * @todo WordPress 4.3发布时移除这个函数
  *
- * @param string $before Optional. Content to prepend to the description. Default empty.
- * @param string $after  Optional. Content to append to the description. Default empty.
+ * @param string $before 可选，在内容之前插入描述
+ * @param string $after 可选，在内容之后插入描述
  */
 function the_archive_description( $before = '', $after = '' ) {
 	$description = apply_filters( 'get_the_archive_description', term_description() );
 
 	if ( ! empty( $description ) ) {
 		/**
-		 * Filter the archive description.
+		 * 文章存档描述
 		 *
 		 * @see term_description()
 		 *
-		 * @param string $description Archive description to be displayed.
+		 * @param string $description 是否显示存档页面描述
 		 */
 		echo $before . $description . $after;
 	}
@@ -222,44 +222,39 @@ function the_archive_description( $before = '', $after = '' ) {
 endif;
 
 /**
- * Returns true if a blog has more than 1 category.
+ * 当有超过一个分类时返回true
  *
  * @return bool
  */
 function _s_categorized_blog() {
 	if ( false === ( $all_the_cool_cats = get_transient( '_s_categories' ) ) ) {
-		// Create an array of all the categories that are attached to posts.
 		$all_the_cool_cats = get_categories( array(
 			'fields'     => 'ids',
 			'hide_empty' => 1,
-
-			// We only need to know if there is more than one category.
 			'number'     => 2,
 		) );
 
-		// Count the number of categories that are attached to the posts.
 		$all_the_cool_cats = count( $all_the_cool_cats );
 
 		set_transient( '_s_categories', $all_the_cool_cats );
 	}
 
 	if ( $all_the_cool_cats > 1 ) {
-		// This blog has more than 1 category so _s_categorized_blog should return true.
+		// 有超过一个分类时， _s_categorized_blog 返回true
 		return true;
 	} else {
-		// This blog has only 1 category so _s_categorized_blog should return false.
+		// 只有一个分类时 _s_categorized_blog 返回false.
 		return false;
 	}
 }
 
 /**
- * Flush out the transients used in _s_categorized_blog.
+ * in _s_categorized_blog.
  */
 function _s_category_transient_flusher() {
 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 		return;
 	}
-	// Like, beat it. Dig?
 	delete_transient( '_s_categories' );
 }
 add_action( 'edit_category', '_s_category_transient_flusher' );
